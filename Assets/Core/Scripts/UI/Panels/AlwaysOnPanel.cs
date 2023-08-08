8cab1226b096297e862bfcd661671ddd73f57e09
Ivanov Grigoriy <grishadmitrievich@gmail.com>
2023-08-09 01:38:54 +0300

    Project Hirarchy Update

--- Assets/Core/Scripts/UI/Panels/AlwaysOnPanel.cs
+++ Assets/Core/Scripts/UI/Panels/AlwaysOnPanel.cs
@@ -2,8 +2,13 @@ using Core.UI;
 using TMPro;
 using UnityEngine;
 
-public class AlwaysOnPanel : Panel
+namespace Core.UI.Panels
 {
-    [SerializeField] private TMP_Text _counter;
-    [SerializeField] private TMP_Text _bestScore;
+    public class AlwaysOnPanel : Panel
+    {
+        [SerializeField] private TMP_Text _counter;
+        [SerializeField] private TMP_Text _bestScore;
+
+
+    }
 }
