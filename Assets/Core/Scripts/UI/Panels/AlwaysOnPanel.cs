2c2c53f05a09cf41bddd2f1c773b2db82842918f
Ivanov Grigoriy <grishadmitrievich@gmail.com>
2023-08-10 21:00:10 +0300

    Flicking every 100 points

--- Assets/Core/Scripts/UI/Panels/AlwaysOnPanel.cs
+++ Assets/Core/Scripts/UI/Panels/AlwaysOnPanel.cs
@@ -1,4 +1,5 @@
 using Gameplay.Interfaces;
+using System.Collections;
 using TMPro;
 using UniRx;
 using UnityEngine;
@@ -11,8 +12,14 @@ namespace Core.UI.Panels
         [SerializeField] private TMP_Text _counter;
         [SerializeField] private TMP_Text _bestScore;
 
+        [SerializeField] private float _flickingPeriod = 0.35f;
+        [SerializeField] private int _flickCount = 4;
+
         private IPointsCounter _pointsCounter;
         private IPlayerProgressHandler _playerProgressHandler;
+
+        private Coroutine _flicking;
+        private int _flickCounter;
 
         [Inject]
         public void Construct(IPointsCounter pointsCounter, IPlayerProgressHandler playerProgressHandler)
@@ -28,7 +35,35 @@ namespace Core.UI.Panels
             DispalyValueOn(_bestScore, _playerProgressHandler.PlayerProgress.Progeress.Value);
 
             _playerProgressHandler.PlayerProgress.Progeress.Subscribe((value) => DispalyValueOn(_bestScore, value));
-            _pointsCounter.Score.Subscribe((value) => DispalyValueOn(_counter, value));
+            _pointsCounter.Score.Subscribe(
+                (value) =>
+                {
+                    if (_flicking != null)
+                        return;
+
+                    DispalyValueOn(_counter, value);
+
+                    if (value > 0 && value % 100 == 0)
+                        _flicking = StartCoroutine(Flick());
+                });
+
+        }
+
+        private IEnumerator Flick()
+        {
+            yield return new WaitForSecondsRealtime(_flickingPeriod);
+            _counter.gameObject.SetActive(!_counter.gameObject.activeSelf);
+
+            _flickCounter++;
+
+            if (_flickCounter < _flickCount)
+                StartCoroutine(Flick());
+            else
+            {
+                _flickCounter = 0;
+                StopAllCoroutines();
+                _flicking = null;
+            }
         }
 
         private void DispalyValueOn(TMP_Text textField, int value) =>
